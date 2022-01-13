fb306db438790d48f6e7e2c55cced15113429031
Demos <nuralimajids@gmail.com>
2022-01-13 23:20:33 +0700

    up

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -21,13 +21,7 @@
           <a href="#">Home</a>
           <a href="#">Product</a>
           <a href="#">Princing</a>
+          <a href="#" class="login">Log in</a>
         </div>
-				<div class="login">
-					<a href="#" class="login btn btn-primary">Log in</a>
-				</div>
       </div>
     </header>
-
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
