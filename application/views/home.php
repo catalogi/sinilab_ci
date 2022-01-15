cb02c6fea7dfdff7d5743e3a8cc5f05242ed74c8
Demos <nuralimajids@gmail.com>
2022-01-15 16:20:29 +0700

    up home 2

--- application/views/home.php
+++ application/views/home.php
@@ -2,24 +2,63 @@
 	<?php
 		$this->load->view('user/header');
 	?>
-	<div class="bg-container">
-		<div class="container">
-			<div class="pt-5 text-white">
-				<header class="py-5 mt-5">
-					<h1 class="display-4">Transparent Navbar</h1>
-					<p class="lead mb-0">Using Bootstrap 4 and Javascript, create a transparent navbar which changes its style on scroll.</p>
-					<p class="lead mb-0">Snippet by
-						<a href="https://bootstrapious.com" class="text-white">
-							<u>Bootstrapious</u></a>
-					</p>
-				</header>
-				<div class="py-5">
-					<p class="lead">Lorem ipsum dolor sit amet, <strong class="font-weight-bold">consectetur adipisicing </strong>elit. Explicabo consectetur odio voluptatum facere animi temporibus, distinctio tempore enim corporis quam <strong class="font-weight-bold">recusandae </strong>placeat! Voluptatum voluptate, ex modi illum quas nam distinctio.</p>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<div class="wrapper">
+		<div class="bg-container">
+			<div class="container-fluid">
+				<div class="space"></div>
+				<div class="row pages d-flex justify-content-around">
+					<div class="col-md-6">
+						<h1 class="headline">Your Homey-Lab in a Click</h1>
+						<h4 class="textline">SiniLab hadir sebagai sebuah layanan penyedia jasa uji laboratorium dari rumah berbasis aplikasi yang dapat diakses di manapun dan kapanpun</h4>
+					</div>
+					<div class="col-md-6">
+						<img class="agent" src="<?php echo base_url(); ?>assets/img/agent.png" alt="">
+					</div>
+
 				</div>
-				<div class="py-5">
-					<p class="lead">Lorem ipsum dolor sit amet, <strong class="font-weight-bold">consectetur adipisicing </strong>elit. Explicabo consectetur odio voluptatum facere animi temporibus, distinctio tempore enim corporis quam <strong class="font-weight-bold">recusandae </strong>placeat! Voluptatum voluptate, ex modi illum quas nam distinctio.</p>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div class="bg-containers">
+			<div class="space2"></div>
+			<div class="container containers">
+				<div class="row d-flex justify-content-center">
+					<div class="main-content">
+						<h1 class="headline">WHY CHOOSE US ?</h1>
+						<h4 class="textline">SiniLab dapat meningkatkan kenyamanan, kemudahan, dan fleksibilitas user untuk mengakses laboratorium dan melakukan pengujian melalui test-kit secara remote dengan rekomendasi tindakan lanjut berbasis machine learning</h4>
+					</div>
+				</div>
+			</div>
+			<div class="row d-flex justify-content-center">
+				<div class="col-md-3">
+				<div class="card-item">
+					<i class="fas fa-user-friends fa-3x uuse"></i>
+					<h3>Penyedia Layanan Jasa Terpercaya</h3>
+					<h4>Hasil tes oleh Laboratorium dan Rumah Sakit terpercaya dengan sampel yang bisa diambil kapan saja di mana saja</h4>
+
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card-item">
+					<i class="fas fa-chart-line fa-3x uuse"></i>
+					<h3>Hasil Akurat</h3>
+					<h4>Informasi dan hasil yang akurat untuk user dengan menggunakan teknologi Machine Learning</h4>
+
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card-item">
+					<i class="fas fa-bookmark fa-3x uuse"></i>
+					<h3>Harga yang Terjangkau</h3>
+					<h4>Laboratorium dan Rumah Sakit terdekat menjadi pilihan utama untuk anda mendapatkan harga terbaik</h4>
+
+				</div>
+			</div>
+			<!-- </div> -->
+		</div>
+		<div class="bg-container">
+			<div class="bg-image container containers">
+				<div class="row d-flex justify-content-start">
+
 				</div>
 			</div>
 		</div>
--- application/views/user/header.php
+++ application/views/user/header.php
@@ -6,11 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Sinilab || </title>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/test.css">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+	<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"> -->
 	<!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css"> -->
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://kit.fontawesome.com/6e99d705ea.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
